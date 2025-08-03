1d4af10997bfbcf965926736a1a6d3f2219c1579
beepingdog <teesrey@outlook.com>
2025-08-02 17:39:10 -0700

    Add username and implemented finder.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Run the command as ./finder.sh filesdir searchstr"
+    exit 1
+fi
+
+filesdir="$1"
+searchstr="$2"
+
+if [ ! -d "$filesdir" ]; then
+    echo "Error '$filesdir' is not an existing valid directory"
+    exit 1
+fi
+
+
+
+numfiles=$(find $filesdir -type f | wc -l)
+numlines=$(grep -r $searchstr | wc -l)
+
+
+
+echo "The number of files are $numfiles and the number of matching lines are $numlines"
+exit 0
